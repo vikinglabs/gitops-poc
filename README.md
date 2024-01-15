4cf67beffc73f9458aa0d39b6224ff3f7aaa6f8f
klaatu51 <lmctague@pm.me>
2024-01-15 13:34:30 +0000

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # OMS GitOps POC
+
+DO THIS FIRST!
+
+1. Login to ECR.
+```
+```
+
+Namespace/oms-core-apis is part of applications argocd/device-usage-api-development and sap-backend-api-development
--- start.sh
+++ start.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 set -e
 
-## Create registry container unless it already exists
-#reg_name='kind-registry'
-#reg_port='5001'
-#if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
-#  docker run \
-#    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --network bridge --name "${reg_name}" \
-#    registry:2
-#fi
-#
-## Create Kind cluster
-#cat <<EOF | kind create cluster --config=-
-#kind: Cluster
-#apiVersion: kind.x-k8s.io/v1alpha4
-#name: local
-#containerdConfigPatches:
-#- |-
-#  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-#    endpoint = ["http://${reg_name}:5000"]
-#nodes:
-#- role: control-plane
-#- role: worker
-#- role: worker
-#EOF
-#
-## Add the registry config to the nodes
-#REGISTRY_DIR="/etc/containerd/certs.d/localhost:${reg_port}"
-#for node in $(kind get nodes); do
-#  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
-#  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
-#[host."http://${reg_name}:5000"]
-#EOF
-#done
-#
-## Connect the registry to the cluster network if not already connected
-#if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
-#  docker network connect "kind" "${reg_name}"
-#fi
-#
-## Document the local registry
-#cat <<EOF | kubectl apply -f -
-#apiVersion: v1
-#kind: ConfigMap
-#metadata:
-#  name: local-registry-hosting
-#  namespace: kube-public
-#data:
-#  localRegistryHosting.v1: |
-#    host: "localhost:${reg_port}"
-#    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
-#EOF
+# Create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --network bridge --name "${reg_name}" \
+    registry:2
+fi
+
+# Create Kind cluster
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: local
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:5000"]
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+
+# Add the registry config to the nodes
+REGISTRY_DIR="/etc/containerd/certs.d/localhost:${reg_port}"
+for node in $(kind get nodes); do
+  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+[host."http://${reg_name}:5000"]
+EOF
+done
+
+# Connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+# Document the local registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 
 # Pull docker images
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 586495777821.dkr.ecr.us-east-1.amazonaws.com
